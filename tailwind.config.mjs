ed0511d83d4123b6fcc319d891e1c32864b94eab
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-06-08 10:07:01 +0200

    Add tailwind fluid typography - refactor pages to use fluid fonts - fix spacing and paddings for small screen

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import fluid, { extract, screens, fontSize } from 'fluid-tailwind'
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: {
+		files: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+		extract,
+	},
 	theme: {
+		screens,
+		fontSize,
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		fluid
+	],
 }
