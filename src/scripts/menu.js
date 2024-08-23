80e9b31dde9ecf4af76142e12aaed9b4dd562a81
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-08-23 15:15:24 +0200

    Rewrite the menu script for better accessibility. - Add more content on the about page - fix small style adjustment

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,55 @@
-const menuButton = document.getElementById('navbar-burger');
-menuButton?.addEventListener('click', () => {
-  [...document.querySelectorAll('.navbar-menu')].forEach((Element) => {
+const menuButton = document.getElementById("navbar-burger");
+const menuContainer = document.getElementById("mobile-menu");
+const navbarMenus = document.querySelectorAll(".navbar-menu");
+const navbarClose = document.querySelector(".navbar-close");
+const body = document.body;
+
+function toggleMenu() {
+  navbarMenus.forEach((Element) => {
     Element.classList.toggle("hidden");
+  });
+
+  if (menuContainer.classList.contains("hidden")) {
+    body.classList.remove("overflow-hidden");
+  } else {
+    body.classList.add("overflow-hidden");
+
+    // Move focus to the first menu item (a link or a button)
+    const firstFocusableElement =
+      menuContainer.querySelector("a[href], button");
+
+    if (firstFocusableElement) {
+      firstFocusableElement.focus();
+    }
+  }
+}
+
+menuButton?.addEventListener("click", (event) => {
+  toggleMenu();
+  event.stopPropagation();
 });
+
+navbarClose?.addEventListener("click", (event) => {
+  toggleMenu();
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !menuContainer.contains(event.target) &&
+    !menuButton.contains(event.target)
+  ) {
+    navbarMenus.forEach((Element) => {
+      Element.classList.add("hidden");
+    });
+    body.classList.remove("overflow-hidden");
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    navbarMenus.forEach((Element) => {
+      Element.classList.add("hidden");
+    });
+    body.classList.remove("overflow-hidden");
+  }
 });
